73feb90f24f8116606809f74e2d76322fd2030ab
Clarissa Mobley <clarissamobley@me.com>
2024-05-22 14:16:23 -0400

    added employee seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,13 @@ VALUES  ('Sales Lead', 100000, 1),
         ('Accountant', 125000, 3),
         ('Legal Team Lead', 250000, 4),
         ('Lawyer', 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('John', 'Doe', 1, NULL),
+        ('Mike', 'Chan', 1, 1),
+        ('Ashley', 'Rodriguez', 2, NULL),
+        ('Kevin', 'Tupik', 2, 3),
+        ('Kunal', 'Singh', 3, NULL),
+        ('Malia', 'Brown', 3, 5),
+        ('Sarah', 'Lourd', 4, NULL),
+        ('Tom', 'Allen', 4, 7);
