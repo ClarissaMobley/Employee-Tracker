179c74db3f01d83ce5fd80b3094c36f24deee342
Clarissa Mobley <clarissamobley@me.com>
2024-05-22 13:15:45 -0400

    added main menu inquirer and switch statements

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const inquirer = require("inquirer");
 const { Pool } = require("pg");
 
 const PORT = process.env.PORT || 3001;
@@ -16,13 +17,64 @@ const pool = new Pool({
 });
 
 pool.connect((err) => {
-    if (err) {
-        console.error('Error connecting to emplyees_db', err.message);
-        return;
-    }
-    console.log('Connected to employees_db');
+  if (err) {
+    console.error("Error connecting to emplyees_db", err.message);
+    return;
+  }
+  console.log("Connected to employees_db");
 });
 
+function mainMenu() {
+  inquirer
+    .prompt({
+      name: "menu",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "Add a Department",
+        "Add a Role",
+        "Add An Employee",
+        "Update An Employee Role",
+        "Quit"
+      ],
+    })
+    .then((answer) => {
+      switch (answer.menu) {
+        case "View All Departments":
+          viewDeparments();
+          break;
+        case "View All Roles":
+            viewRoles();
+            break;
+        case "View All Employees":
+            viewEmployees();
+            break;
+        case "Add a Department":
+            addDepartment();
+            break;
+        case "Add a Role":
+            addRole();
+            break;
+        case "Add An Employee":
+            addEmployee();
+            break;
+        case "Update An Employee Role":
+            updateEmployee();
+            break;
+        case "Quit":
+            console.log("Bye!");
+            pool.exit();
+            break;
+        default:
+            console.log('Invalid');
+            mainMenu();
+      }
+    });
+}
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
